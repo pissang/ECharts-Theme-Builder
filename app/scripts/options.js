af5435f50999d189ce884b12688a1dee914ea8e9
pissang <bm2736892@gmail.com>
2020-06-24 16:28:34 +0800

    tweak pie data

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -398,7 +398,7 @@ function getOptions(vm) {
   legend: {
       orient: 'vertical',
       left: 'left',
-      data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+      data: ['搜索引擎', '直接访问', '邮件营销', '联盟广告', '视频广告']
   },
   series: [
       {
@@ -407,11 +407,11 @@ function getOptions(vm) {
           radius: '55%',
           center: ['50%', '60%'],
           data: [
+                {value: 1000, name: '搜索引擎'},
               {value: 335, name: '直接访问'},
               {value: 310, name: '邮件营销'},
               {value: 234, name: '联盟广告'},
-              {value: 135, name: '视频广告'},
-              {value: 1548, name: '搜索引擎'}
+              {value: 135, name: '视频广告'}
           ],
           emphasis: {
               itemStyle: {
