434eb26a0dd53b514f82f2575c18253f50c63a15
pissang <bm2736892@gmail.com>
2020-06-24 16:35:23 +0800

    add bar on polar

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -527,6 +527,77 @@ function getOptions(vm) {
 },
 
 
+////////////////////// 极坐标角度柱状图 ////////////////////////////////
+{
+    angleAxis: {
+    },
+    radiusAxis: {
+        type: 'category',
+        data: ['周一', '周二', '周三', '周四'],
+        z: 10
+    },
+    polar: {
+    },
+    series: [{
+        type: 'bar',
+        data: [1, 2, 3, 4],
+        coordinateSystem: 'polar',
+        name: 'A',
+        stack: 'a'
+    }, {
+        type: 'bar',
+        data: [2, 4, 6, 8],
+        coordinateSystem: 'polar',
+        name: 'B',
+        stack: 'a'
+    }, {
+        type: 'bar',
+        data: [1, 2, 3, 4],
+        coordinateSystem: 'polar',
+        name: 'C',
+        stack: 'a'
+    }],
+    legend: {
+        show: true,
+        data: ['A', 'B', 'C']
+    }
+},
+
+////////////////////// 极坐标径向柱状图 ////////////////////////////////
+{
+    angleAxis: {
+        type: 'category',
+        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+    },
+    radiusAxis: {
+    },
+    polar: {
+    },
+    series: [{
+        type: 'bar',
+        data: [1, 2, 3, 4, 3, 5, 1],
+        coordinateSystem: 'polar',
+        name: 'A',
+        stack: 'a'
+    }, {
+        type: 'bar',
+        data: [2, 4, 6, 1, 3, 2, 1],
+        coordinateSystem: 'polar',
+        name: 'B',
+        stack: 'a'
+    }, {
+        type: 'bar',
+        data: [1, 2, 3, 4, 1, 2, 5],
+        coordinateSystem: 'polar',
+        name: 'C',
+        stack: 'a'
+    }],
+    legend: {
+        show: true,
+        data: ['A', 'B', 'C']
+    }
+},
+
 
 ////////////////////// Treemap ////////////////////////////////
 (function () {
